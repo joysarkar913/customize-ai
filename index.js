d218a315410874c7952cc92a71e6ee730a45005b
joysarkar913 <76178347+joysarkar913@users.noreply.github.com>
2026-09-14 00:15:47 +0530

    Version 2.0.2.0

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ app.post("/letter_writer", async (quries, responses) => {
 app.post("/math_solution", async (quries, responses) => {
   const data = quries.headers;
   const criteria = quries.body;
-  const genProms = `Generate a proper solution  of the question is \[ ${criteria.question} \] based on context. Provide the output strictly in JSON format with no conversational text. full html and css data set : {}this object field.. write in html with embedded css format use insde the html and css with remark of every solution format must look like as hand written , if it is math question provide answer with comment to get understanding in concept . ${process.env.CUSTOMIZE_RES}`
+  const genProms = `Generate a proper solution  of the question is \[ ${criteria.question} \] based on context. Provide the output strictly in JSON format with no conversational text. full html and css data set : {}this object with out any field.. write in html with embedded css format use insde the html and css with remark of every solution format must look like as hand written , if it is math question provide answer with comment to get understanding in concept . ${process.env.CUSTOMIZE_RES}`
    try {
   const progress = await module1_generater(genProms);
 
